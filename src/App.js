76e9b67946303e9ce5abd390cb328e9eed64838a
vivekhere <vivekjai10@gmail.com>
2021-02-12 21:23:33 +0530

    optimistic vs pessimistic updates

--- src/App.js
+++ src/App.js
@@ -36,11 +36,18 @@ class App extends Component {
   };
 
   handleDelete = async (post) => {
-    await axios.delete(apiEndpoint + "/" + post.id); // delete post from server
+    const originalPosts = this.state.posts;
 
-    // remove post from table
+    // remove post from table / UI
     const posts = this.state.posts.filter((p) => p.id !== post.id);
     this.setState({ posts });
+
+    try {
+      await axios.delete(apiEndpoint + "/" + post.id); // delete post from server
+    } catch (ex) {
+      alert("Something failed while deleting a post!");
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
@@ -87,3 +94,25 @@ class App extends Component {
 }
 
 export default App;
+
+// Optimistic vs Pessimistic updates
+
+// Pessimistic update
+// We have implemented all the 'CRUD' operations there is a small issue in our
+// previous implementation. Note that when we click on the delete button it takes
+// about a second or half a second until the post is removed from the table.
+// We have the same issue when adding a post or updating a post. There is a delay
+// until we see the result.
+// The reason is our previous implementation we are calling the server first and
+// then updating the view or the UI.
+// With this implementation when an error occurs while calling the server the
+// rest of the function will not be executed. This is what we call a pessimistic
+// update. So, we are not sure if a call to the server is going to succeed or fail.
+
+// Pessimistic update
+// We can assume most of the time the call to the server succeeds. So, instead of
+// calling the server first which is going to take some time we gonna go ahead
+// and update the UI and then call the server.
+// If this call fails revert the UI back to the previous state. In React this is
+// easy because we never update the state directly so we can always have a
+// reference to the previous state.
