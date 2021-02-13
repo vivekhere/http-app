966984c0200d089ad0b78b2ade80cc4d07701feb
vivekhere <vivekjai10@gmail.com>
2021-02-13 21:17:38 +0530

    logging errors

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -7,10 +7,7 @@ axios.interceptors.response.use(null, (error) => {
     error.response.status >= 400 &&
     error.response.status < 500;
 
-  if (!expectedError) {
-    console.log("Logging the error", error);
-    toast("An unexpected error occurred.");
-  }
+  if (!expectedError) toast("An unexpected error occurred.");
 
   // to pass control to our catch block return a rejected promise
   return Promise.reject(error);
@@ -22,3 +19,13 @@ export default {
   put: axios.put,
   delete: axios.delete,
 };
+
+// Logging errors
+
+// Here in our httpService currently if we have an error we log it in the console.
+// This is fine on a development machine but when we deploy our application to
+// production evvironment, this is not going to be helpful because this console
+// is part of the end users browser and we don't have access to that console.
+// So, we need to store this error or log it somewhere where we can access and
+// that is when we use Logging as a Service provider.
+// sentry.io is the most famous logging as a service provider.
